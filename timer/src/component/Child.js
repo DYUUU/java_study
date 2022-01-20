fea3a649f8d83479882f4287d2280db0419916fd
Karl <jae637@naver.com>
2022-01-20 23:53:20 +0900

    변수 선언

--- timer/src/component/Child.js
+++ timer/src/component/Child.js
@@ -1,25 +1,13 @@
-function Child({ contents }) {
-  let arr = [3, 5, 1, 2, 4];
-  let obj = { 1: { "index": 3, "value": 1 }, 2: 4, 3: 1, 4: { "index": 5, "value": 4 }, 5: 2 };
+import React from "react";
 
-  function find(number) {
-    let ai = -1;
-    let oi = -1;
-    for (let i = 0; i < 5; i++) {
-      if (arr[i] == number) {
-        ai = i + 1;
-        break;
-      }
-    }
-    return ai;
-  }
+function Child({ contents }) {
+  const [a, setA] = React.useState("hi");
 
   return (
     <div className="App">
       <span>내용:</span>
-      {find(4)}
-      {obj[4]["index"]}
-      {contents}
+      {a}
+      <input type="button" value="button" onClick={() => { setA("바꿈") }}></input>
     </div>
   );
 }
