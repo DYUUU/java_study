5ececf2395cf73574dd68c3ce266a24880539c25
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-01-21 21:54:14 +0900

    알고리즘 공부

--- timer/src/App.js
+++ timer/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Child  from './component/Child';
 
 function App() {
   return (
     <div className="App">
       Hello World
+      <Child contents="Hello World"/>
     </div>
   );
 }
---
+++ timer/src/component/Child.js
@@ -0,0 +1,9 @@
+function Child({contents}) {
+  return (
+    <div className="App">
+      {contents}
+    </div>
+  );
+}
+
+export default Child;
