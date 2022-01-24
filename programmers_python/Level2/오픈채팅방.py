e13f82346477c19747e426e362c2a5b57b0a84fa
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-01-24 12:54:17 +0900

    정답률 25%
    모르겠음... 어디서 틀린  건지

--- programmers_python/Level2/오픈채팅방.py
+++ programmers_python/Level2/오픈채팅방.py
@@ -2,6 +2,7 @@ def solution(record):
     answer = []
     userlist = {}
     for userlistStr in record:
+        # 유저 추가하기
         tmp = userlistStr.split()
         if(tmp[0]=="Enter"):
             answer.append(tmp[1]+"님이 들어왔습니다.")
@@ -10,7 +11,14 @@ def solution(record):
             userlist[tmp[1]]=tmp[2]
         elif(tmp[0]=="Leave"):
             answer.append(tmp[1]+"님이 나갔습니다.")
-    for i in range(0,len(answer)):
-        for usr in userlist.keys():
+    # 유저 이름 변경하기 (후처리)
+    for usr in userlist.keys():
+        for i in range(0,len(answer)):
             answer[i]=answer[i].replace(usr,userlist.get(usr))
+    # 왜 안 되는 걸까요?........
+    print(answer)
+    print(userlist)
     return answer
+
+record=["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+solution(record)
