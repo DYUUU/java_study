360f5b8726a8d55d3fb79324504f77b9a2789b18
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-01-24 12:42:10 +0900

    정확성 25%

--- programmers_python/Level2/오픈채팅방.py
+++ programmers_python/Level2/오픈채팅방.py
@@ -1,17 +1,16 @@
 def solution(record):
     answer = []
     userlist = {}
-    for str in record:
-        tmp = str.split()
+    for userlistStr in record:
+        tmp = userlistStr.split()
         if(tmp[0]=="Enter"):
-            answer.append(tmp[2]+"님이 들어왔습니다.")
+            answer.append(tmp[1]+"님이 들어왔습니다.")
             userlist[tmp[1]]=tmp[2]
         elif(tmp[0]=="Change"):
             userlist[tmp[1]]=tmp[2]
         elif(tmp[0]=="Leave"):
-            answer.append(userlist.get(tmp[1])+"님이 나갔습니다.")
-    print(answer)
+            answer.append(tmp[1]+"님이 나갔습니다.")
+    for i in range(0,len(answer)):
+        for usr in userlist.keys():
+            answer[i]=answer[i].replace(usr,userlist.get(usr))
     return answer
-
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
-solution(record)
