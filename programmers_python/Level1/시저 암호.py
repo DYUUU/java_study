13742cdceb0aff59b9877d4f94f7f52ed2068cc8
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-01-25 11:31:27 +0900

    정답률 46.2%

--- programmers_python/Level1/시저 암호.py
+++ programmers_python/Level1/시저 암호.py
@@ -1,14 +1,31 @@
+"""
+ord() 문자열->아스키
+chr() 아스키->문자열
+"""
+
 def solution(s, n):
     answer = ''
-    for i in s :
-        if(i==" "):
+    for str in s :
+        # 공백 처리
+        if(str==" "):
             answer += " "
             continue
-        if()
-        answer += 
+        asciiValue = ord(str) + n
+        # 대문자
+        if(ord(str)>=ord("A") and ord(str) <= ord("Z")):
+            if(asciiValue >= ord("Z")):
+                answer += chr(asciiValue - 26)
+            else:
+                answer += chr(asciiValue)
+        # 소문자
+        elif(ord(str)>=ord("a") and ord(str) <= ord("z")):
+            if(asciiValue >= ord("z")):
+                answer += chr(asciiValue - 26)
+            else:
+                answer += chr(asciiValue)
     print(answer)
     return answer
 
-s="AB"
-n=1
+s="X Z z"
+n=25
 solution(s,n)
