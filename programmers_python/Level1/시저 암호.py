da42ad42aaee6686174493019cdeb180f5a147bb
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-01-25 10:46:26 +0900

    test

---
+++ programmers_python/Level1/시저 암호.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    answer = ''
+    for i in s :
+        if(i==" "):
+            answer += " "
+            continue
+        if()
+        answer += 
+    print(answer)
+    return answer
+
+s="AB"
+n=1
+solution(s,n)
