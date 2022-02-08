d88e0f143fd696f416cc314e7931e93ef1ec194c
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-08 22:05:32 +0900

    -

---
+++ java_study/programmers_java/Level1/스킬 체크/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Solution1 sol1 = new Solution1();
+        sol1.solution(12);
+        Solution2 sol2 = new Solution2();
+        sol2.solution(
+                new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5}
+                ,"right");
+    }
+}
