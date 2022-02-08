c26e25cbc2f2925751f98a39bcaa20ea5811f59d
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-08 23:13:27 +0900

    스킬 체크 실패!!

--- java_study/programmers_java/Level1/정수 내림차순으로 배치하기/src/com/company/Solution.java
+++ java_study/programmers_java/Level1/정수 내림차순으로 배치하기/src/com/company/Solution.java
@@ -1,12 +1,12 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solution {
     public long solution(long n) {
         String str = Long.toString(n);
         char[] tmp = str.toCharArray();
-        Arrays.sort(tmp);
 
         return Long.valueOf(new StringBuilder(new String(tmp)).reverse().toString());
     }
