20b8542a10ab79c6f4daeaa49907ba1189162673
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-09 15:25:12 +0900

    해결 완료!!

--- java_study/programmers_java/Level1/스킬 체크/src/com/company/Main.java
+++ java_study/programmers_java/Level1/스킬 체크/src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         sol1.solution(12);
         Solution2 sol2 = new Solution2();
         sol2.solution(
-                new int[]{1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5}
-                ,"right");
+                new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}
+                ,"left");
     }
 }
--- java_study/programmers_java/Level1/스킬 체크/src/com/company/Solution2.java
+++ java_study/programmers_java/Level1/스킬 체크/src/com/company/Solution2.java
@@ -1,61 +1,93 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Solution2 {
+
+    public class Left {
+        int row = 3;
+        int col = 0;
+        int abs = 0;
+
+        public void coordinateChange(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public void setAbs(int row, int col) {
+            this.abs = Math.abs(row - this.row) + Math.abs(col - this.col);
+        }
+    }
+
+    public class Right {
+        int row = 3;
+        int col = 2;
+        int abs = 0;
+
+        public void coordinateChange(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        public void setAbs(int row, int col) {
+            this.abs = Math.abs(row - this.row) + Math.abs(col - this.col);
+        }
+    }
+
+
     public String solution(int[] numbers, String hand) {
         String answer = "";
-        int left = 0;
-        int right = 0;
-        for(int i = 0; i <numbers.length;i++)
-        {
-            if( numbers[i] == 1 ||
-                numbers[i] == 4 ||
-                numbers[i] == 7)
-            {
-                answer+="L";
-                left = numbers[i];
-            }
-            else if(numbers[i] == 3 ||
-                    numbers[i] == 6 ||
-                    numbers[i] == 9)
-            {
-                answer+="R";
-                right = numbers[i];
-            }
-            else if(numbers[i] == 2 ||
-                    numbers[i] == 5 ||
-                    numbers[i] == 8 ||
-                    numbers[i] == 0)
-            {
 
-                int leftabs = Math.abs(numbers[i]-left);
-                int rightabs = Math.abs(numbers[i]-right);
-                if(leftabs>rightabs)
-                {
-                    answer+="R";
-                    right=numbers[i];
+        Left left = new Left();
+        Right right = new Right();
+
+        Integer[][] intArray = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 0, 11}
+        };
+
+        for (int i : numbers) {
+            for (int row = 0; row < 4; row++) {
+                int col = Arrays.asList(intArray[row]).indexOf(i);
+
+                if (col < 0)
+                    continue;
+                // 좌측일 경우
+                else if (col == 0) {
+                    answer += "L";
+                    left.coordinateChange(row, col);
                 }
-                if(leftabs<rightabs)
-                {
-                    answer+="L";
-                    left=numbers[i];
-                }
-                if(leftabs==rightabs)
-                {
-                    if(hand.equals("right"))
-                    {
-                        answer+="R";
-                        right=numbers[i];
-                    }
-                    else if(hand.equals("left"))
-                    {
-                        answer+="L";
-                        left=numbers[i];
+                // 가운데일 경우
+                else if (col == 1) {
+                    right.setAbs(row, col);
+                    left.setAbs(row, col);
+
+                    if (right.abs > left.abs) {
+                        answer += "L";
+                        left.coordinateChange(row, col);
+                    } else if (right.abs < left.abs) {
+                        answer += "R";
+                        right.coordinateChange(row, col);
+                    } else {
+                        if (hand.equals("right")) {
+                            answer += "R";
+                            right.coordinateChange(row, col);
+                        } else if (hand.equals("left")) {
+                            answer += "L";
+                            left.coordinateChange(row, col);
+                        }
                     }
                 }
-
+                // 우측일 경우
+                else if (col == 2) {
+                    answer += "R";
+                    right.coordinateChange(row, col);
+                }
             }
         }
-        System.out.println(answer);
+
         return answer;
     }
 }
