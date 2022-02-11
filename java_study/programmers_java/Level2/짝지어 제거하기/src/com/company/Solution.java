0aad4ae8b67084550932b35a883b2e8573d32080
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-11 12:33:25 +0900

    다른 방식 왜 안되지..

--- java_study/programmers_java/Level2/짝지어 제거하기/src/com/company/Solution.java
+++ java_study/programmers_java/Level2/짝지어 제거하기/src/com/company/Solution.java
@@ -1,30 +1,30 @@
 package com.company;
 
 import java.util.Arrays;
-
 public class Solution {
     public int solution(String s) {
         int[] alphabet = new int[26];
-        boolean flag = false;
+        int dist = 0;
 
         if (s.length() % 2 != 0) return 0;
         else {
             for (int i = 0; i < s.length(); i++) {
-                alphabet[s.charAt(i) - 'a']++;
-                if (i + 1 == s.length()) break;
-                if (s.charAt(i) == s.charAt(i + 1)) {
-                    flag = true;
+                if (alphabet[s.charAt(i) - 'a'] == 0)
+                    alphabet[s.charAt(i) - 'a'] = i ;
+                else {
+                    dist = Math.abs(alphabet[s.charAt(i) - 'a'] - i);
+                    if (dist == 1)
+                    {
+                        alphabet[s.charAt(i) - 'a'] = 0;
+                        continue;
+                    }
+                    else if (dist % 2 == 0)
+                        return 0;
+                    else
+                        alphabet[s.charAt(i) - 'a'] = 0;
                 }
             }
-       }
-        if (flag) {
-            for(int j = 0 ; j < alphabet.length;j++)
-            {
-                if(alphabet[j]%2!=0)
-                    return 0;
-            }
-            return 1;
-        } else
-            return 0;
+        }
+        return 1;
     }
 }
