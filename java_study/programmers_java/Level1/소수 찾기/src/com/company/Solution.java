a379d5c2e135392d251c16a1c2a982ef764cb371
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-12 20:10:25 +0900

    여쭤볼 것!

--- java_study/programmers_java/Level1/소수 찾기/src/com/company/Solution.java
+++ java_study/programmers_java/Level1/소수 찾기/src/com/company/Solution.java
@@ -5,18 +5,28 @@ import java.util.ArrayList;
 
 class Solution {
     public int solution(int n) {
+//        ArrayList<Integer> arrList = new ArrayList<>();
+//
+//        for (int i = 2; i <= n; i++) {
+//            arrList.add(i);
+//        }
+//
+//        // 에라토스테네스의 체 안됨 ㅠ
+//        for (int i = 0; i < arrList.size(); i++) {
+//            for (int j = arrList.get(i); j <= n / arrList.get(i); j++) {
+//                arrList.remove(Integer.valueOf(arrList.get(i) * j));
+//            }
+//        }
+
+        // 정수론 실패!! ㅠㅠ 도저히 몰겟삼~~~
         ArrayList<Integer> intArr = new ArrayList<>();
         boolean flag = true;
 
-        intArr.add(2);
-
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             for (int j = 0; j < intArr.size(); j++) {
                 if (i % intArr.get(j) == 0) {
-                    if (i != intArr.get(j)) {
                         flag = false;
                         break;
-                    }
                 }
             }
             if (flag) {
@@ -25,6 +35,7 @@ class Solution {
             } else
                 flag = true;
         }
+
 
         return intArr.size();
     }
