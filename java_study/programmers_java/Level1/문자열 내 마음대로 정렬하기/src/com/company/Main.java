56bbb2d25981d29384f57da28ff9e21bc4bf22fd
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-14 12:37:30 +0900

    해결 완료!

--- java_study/programmers_java/Level1/문자열 내 마음대로 정렬하기/src/com/company/Main.java
+++ java_study/programmers_java/Level1/문자열 내 마음대로 정렬하기/src/com/company/Main.java
@@ -5,14 +5,12 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         String[] strings = {"sun", "bed", "car"};
-        String[] strs= {"abce", "abcd", "cdx"};
+        String[] strs = {"abce", "abcd", "cdx"};
         int n = 1;
-        int n2=2;
+        int n2 = 2;
 
         Solution sol = new Solution();
-        Solution2 sol2 = new Solution2();
         sol.solution(strings, n);
-        sol2.solution(strings,n);
-        sol2.solution(strs,n2);
+        sol.solution(strs, n2);
     }
 }
--- java_study/programmers_java/Level1/문자열 내 마음대로 정렬하기/src/com/company/Solution.java
+++ java_study/programmers_java/Level1/문자열 내 마음대로 정렬하기/src/com/company/Solution.java
@@ -1,48 +1,25 @@
 package com.company;
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
-        ArrayList<String> str = new ArrayList<>();
-        for (int i = 0; i < strings.length; i++) {
-            if (str.size() == 0) {
-                str.add(strings[i]);
-                continue;
-            }
-            for (int j = 0; j < str.size(); j++) {
-                if (strings[i].charAt(n) < str.get(j).charAt(n)) {
-                    for(int k = str.size(); k > 0;k--) {
-                        if (k >= 2) {
-                            str.add(str.get(k - 1));
-                            str.set(k - 1, str.get(k - 2));
-                        }
-                        else {
-                            str.set(0, strings[i]);
-                        }
-                    }
+        String[] answer = strings;
 
-                } else if (strings[i].charAt(n) == str.get(j).charAt(n)) {
-                    if (strings[i].charAt(n + 1) < str.get(j).charAt(n + 1)) {
-                        str.add( str.get(j));
-                        str.set(0, strings[i]);
-                        break;
-                    }
-                    else {
-                        str.add(strings[i]);
-                        break;
-                    }
-                } else {
-                    str.add(strings[i]);
-                    break;
+        Arrays.sort(answer, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.charAt(n)==o2.charAt(n)) {
+                    return o1.compareTo(o2);
                 }
+                return o1.charAt(n)-o2.charAt(n);
             }
-        }
+        });
 
-        System.out.println(str);
+        System.out.println(Arrays.toString(answer));
+
 
         return answer;
     }
