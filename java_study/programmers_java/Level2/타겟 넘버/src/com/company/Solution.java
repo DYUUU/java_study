ada7c9e4cdf16f7879b5983f802ca9e501b98b5d
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-15 01:00:31 +0900

    왜 안되지...

---
+++ java_study/programmers_java/Level2/타겟 넘버/src/com/company/Solution.java
@@ -0,0 +1,29 @@
+package com.company;
+
+class Solution {
+    public int[] copyNumbers;
+    public int copyTarget;
+    public int count;
+    public int cnt = 0;
+
+    public void DFS(int size, int current) {cnt++;
+        if (current == copyNumbers.length - 1) {
+            if (copyTarget == size) {
+                count += 1;
+            }
+        } else {
+            DFS(size + copyNumbers[current], current + 1);
+            DFS(size - copyNumbers[current], current + 1);
+        }
+    }
+
+    public int solution(int[] numbers, int target) {
+        copyNumbers = numbers.clone();
+        copyTarget = target;
+
+        DFS(0, 0);
+        System.out.println(count);
+        System.out.println(cnt);
+        return count;
+    }
+}
