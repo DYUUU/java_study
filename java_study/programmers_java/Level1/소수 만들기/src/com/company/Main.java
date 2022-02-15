e36288bfe3e9a887bb0678cbb8fe6e41fe16fc4c
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-15 15:26:15 +0900

    왜 시간 초과지..

--- java_study/programmers_java/Level1/소수 만들기/src/com/company/Main.java
+++ java_study/programmers_java/Level1/소수 만들기/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] nums = {1,2,3,4};
+        int[] nums = {1, 2, 7, 6, 4};
         Solution sol = new Solution();
 
         sol.solution(nums);
--- java_study/programmers_java/Level1/소수 만들기/src/com/company/Solution.java
+++ java_study/programmers_java/Level1/소수 만들기/src/com/company/Solution.java
@@ -1,39 +1,34 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class Solution {
-    public int currentNum = 0;
+
     public int count = 0;
 
-    public void BFS(int[] nums, int current, int threeCnt, int size, boolean[] prime) {
+    public void DFS(int[] nums, int current, int threeCnt, int size, boolean[] prime) {
         if (threeCnt == 3 && prime[size] == false) {
-            if(current<=nums.length)
-               count++;
+            count++;
         } else if (current < nums.length) {
-            BFS(nums, current + 1, threeCnt + 1, size + nums[current], prime);
-            BFS(nums, current + 2, threeCnt + 1, size + nums[current], prime);
-        } else if (current == nums.length) {
-            currentNum += 1;
-            BFS(nums, currentNum, 0, 0, prime);
+            DFS(nums, current + 1, threeCnt + 1, size + nums[current], prime);
+            DFS(nums, current + 1, threeCnt, size, prime);
         }
 
     }
 
     public int solution(int[] nums) {
         Arrays.sort(nums);
-        int maxNum = nums[nums.length - 1] + nums[nums.length - 2] + nums[nums.length - 3];
+        int maxNum = 1000+999+998;
+
         boolean[] prime = new boolean[maxNum + 1];
-        prime[0] = true;
-        prime[1] = true;
+
         for (int i = 2; i <= Math.sqrt(maxNum); i++) {
             for (int j = i; j <= maxNum / i; j++) {
                 prime[i * j] = true;
             }
         }
 
-        BFS(nums, currentNum, 0, 0, prime);
+        DFS(nums, 0, 0, 0, prime);
 
         System.out.println(count);
 
