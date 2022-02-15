6a94ccdb19c9ad7b3e64fdfdac1a88c8d60fb239
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-15 13:12:52 +0900

    모르겠다..!!

---
+++ java_study/programmers_java/Level1/소수 만들기/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] nums = {1,2,3,4};
+        Solution sol = new Solution();
+
+        sol.solution(nums);
+    }
+}
