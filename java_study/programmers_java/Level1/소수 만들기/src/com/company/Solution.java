1682a1cc10777ffdd37d401bfdaa295b4e621229
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-15 15:55:27 +0900

    DFS 해결 완료

--- java_study/programmers_java/Level1/소수 만들기/src/com/company/Solution.java
+++ java_study/programmers_java/Level1/소수 만들기/src/com/company/Solution.java
@@ -3,32 +3,32 @@ package com.company;
 import java.util.Arrays;
 
 public class Solution {
-
+    boolean[] prime = new boolean[3000];
     public int count = 0;
 
-    public void DFS(int[] nums, int current, int threeCnt, int size, boolean[] prime) {
+    public void DFS(int[] nums, int current, int threeCnt, int size) {
         if (threeCnt == 3 && prime[size] == false) {
             count++;
-        } else if (current < nums.length) {
-            DFS(nums, current + 1, threeCnt + 1, size + nums[current], prime);
-            DFS(nums, current + 1, threeCnt, size, prime);
+        }
+        else if(current==nums.length||threeCnt>3)
+        {
+            return;
+        }
+        else{
+            DFS(nums, current + 1, threeCnt + 1, size + nums[current]);
+            DFS(nums, current + 1, threeCnt, size);
         }
 
     }
 
     public int solution(int[] nums) {
-        Arrays.sort(nums);
-        int maxNum = 1000+999+998;
-
-        boolean[] prime = new boolean[maxNum + 1];
-
-        for (int i = 2; i <= Math.sqrt(maxNum); i++) {
-            for (int j = i; j <= maxNum / i; j++) {
+        for (int i = 2; i <= Math.sqrt(2997); i++) {
+            for (int j = i; j <= 2997 / i; j++) {
                 prime[i * j] = true;
             }
         }
 
-        DFS(nums, 0, 0, 0, prime);
+        DFS(nums, 0, 0, 0);
 
         System.out.println(count);
 
