f0f06a64941671db51ba98aa9c7e6d10c0ec298a
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-18 19:51:50 +0900

    Solution3 해결 완료!

--- java_study/programmers_java/Level2/위장/src/com/company/Solution3.java
+++ java_study/programmers_java/Level2/위장/src/com/company/Solution3.java
@@ -4,11 +4,8 @@ import java.util.HashMap;
 
 public class Solution3 {
 
-
     public int solution(String[][] clothes) {
-        int[][] combination = new int[30][30];
         HashMap<String, Integer> hashMap = new HashMap<>();
-        int combX = 0;
 
         for (int i = 0; i < clothes.length; i++) {
             if (hashMap.containsKey(clothes[i][1]))
