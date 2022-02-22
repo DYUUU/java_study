5d114876cfdb8e416dab29bbe57b0672bec11ce7
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-22 19:36:50 +0900

    정확성 23.1%

--- java_study/programmers_java/Level3/네트워크/src/com/company/Solution.java
+++ java_study/programmers_java/Level3/네트워크/src/com/company/Solution.java
@@ -1,27 +1,27 @@
 package com.company;
 
-
 import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Solution {
-    public ArrayList<Integer> visit;
+    public HashSet<Integer> visit;
     public HashSet<Integer> network = new HashSet<>();
 
     public void findNetwork(int current, ArrayList<String> arr) {
         for (int i = 0; i < arr.get(current).length(); i++) {
-            int tmp = Integer.valueOf(String.valueOf(arr.get(current).charAt(i)));
+            int tmp = Integer.parseInt(String.valueOf(arr.get(current).charAt(i)));
             if (!visit.contains(tmp)) {
                 visit.add(tmp);
-                findNetwork(i, arr);
+                findNetwork(tmp-1, arr);
             }
         }
     }
 
     public int solution(int n, int[][] computers) {
-        visit = new ArrayList<>();
+        visit = new HashSet<>();
         ArrayList<String> arr = new ArrayList<>();
         String str = "";
+
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
