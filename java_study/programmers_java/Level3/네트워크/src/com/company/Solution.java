ed41561f02158c8a23ca3b6a046c3289eb38ff7d
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-22 21:53:09 +0900

    정확성 30.8% 2

--- java_study/programmers_java/Level3/네트워크/src/com/company/Solution.java
+++ java_study/programmers_java/Level3/네트워크/src/com/company/Solution.java
@@ -4,21 +4,21 @@ package com.company;
 import java.util.HashSet;
 
 public class Solution {
-    public HashSet<Integer> visit;
+    public boolean[] visit;
     public HashSet<Integer> network = new HashSet<>();
 
-    public void findNetwork(int current,String[] arr) {
+    public void findNetwork(int current, String[] arr) {
         for (int i = 0; i < arr[current].length(); i++) {
-            int tmp = Integer.parseInt(String.valueOf(arr[current].charAt(i)));
-            if (!visit.contains(tmp)) {
-                visit.add(tmp);
+            int tmp = Integer.valueOf(String.valueOf(arr[current].charAt(i)));
+            if (!visit[tmp]) {
+                visit[tmp] = true;
                 findNetwork(tmp, arr);
             }
         }
     }
 
     public int solution(int n, int[][] computers) {
-        visit = new HashSet<>();
+        visit = new boolean[200];
         String[] arr = new String[n];
         String str = "";
 
@@ -28,12 +28,12 @@ public class Solution {
                 if (computers[i][j] == 1)
                     str += j;
             }
-            arr[i]=(str);
+            arr[i] = (str);
             str = "";
         }
 
         for (int i = 0; i < n; i++) {
-            if (!visit.contains(i)) {
+            if (!visit[i]) {
                 findNetwork(i, arr);
                 network.add(i);
             }
