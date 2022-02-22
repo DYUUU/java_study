753101d7a803db0cda732e47e38a6e2b96a1c0c0
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-22 22:02:09 +0900

    결국 배열로 해결 완료

--- java_study/programmers_java/Level3/네트워크/src/com/company/Solution.java
+++ java_study/programmers_java/Level3/네트워크/src/com/company/Solution.java
@@ -7,34 +7,23 @@ public class Solution {
     public boolean[] visit;
     public HashSet<Integer> network = new HashSet<>();
 
-    public void findNetwork(int current, String[] arr) {
-        for (int i = 0; i < arr[current].length(); i++) {
-            int tmp = Integer.valueOf(String.valueOf(arr[current].charAt(i)));
-            if (!visit[tmp]) {
-                visit[tmp] = true;
-                findNetwork(tmp, arr);
+    public void findNetwork(int current, int[][] computers) {
+        for (int i = 0; i < computers[current].length; i++) {
+            if (!visit[i]) {
+                if (computers[current][i] == 1) {
+                    visit[i] = true;
+                    findNetwork(i, computers);
+                }
             }
         }
     }
 
     public int solution(int n, int[][] computers) {
         visit = new boolean[200];
-        String[] arr = new String[n];
-        String str = "";
-
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (computers[i][j] == 1)
-                    str += j;
-            }
-            arr[i] = (str);
-            str = "";
-        }
 
         for (int i = 0; i < n; i++) {
             if (!visit[i]) {
-                findNetwork(i, arr);
+                findNetwork(i, computers);
                 network.add(i);
             }
         }
