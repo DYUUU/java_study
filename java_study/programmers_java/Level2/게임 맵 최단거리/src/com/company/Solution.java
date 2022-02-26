7071cdd2096fbbe673ba6dd1469e1ba7eab0abf5
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-26 21:26:29 +0900

    10.5점

--- java_study/programmers_java/Level2/게임 맵 최단거리/src/com/company/Solution.java
+++ java_study/programmers_java/Level2/게임 맵 최단거리/src/com/company/Solution.java
@@ -1,88 +1,56 @@
 package com.company;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Solution {
     public int route = 0;
+    public int answer = 0;
 
-    public void findOpenedPlace(int[][] maps, int x, int y, boolean[] isOpened,int count) {
-        // 상
-        if (x - 1 >= 0) {
-            if (maps[x - 1][y] == 1||maps[x - 1][y]>count)
-                isOpened[0] = true;
-        }
-        // 하
-        if (x + 1 <= maps.length - 1) {
-            if (maps[x + 1][y] == 1||maps[x + 1][y]>count)
-                isOpened[1] = true;
-        }
-        // 좌
-        if (y - 1 >= 0 ) {
-            if (maps[x][y - 1] == 1||maps[x][y-1]>count)
-                isOpened[2] = true;
-        }
-        // 우
-        if (y + 1 <= maps.length - 1) {
-            if (maps[x][y + 1] == 1||maps[x][y+1]>count)
-                isOpened[3] = true;
-        }
-    }
-
-    public int changeCoordinate(int[][] maps, int x, int y, boolean[] isOpened, int count) {
-
+    public void changeCoordinate(int[][] maps, int x, int y, int count) {
+        int dst = maps[maps.length - 1][maps.length - 1];
         // 모든 루트를 다 돌았을 때 실패
         if (count == route - 1) {
-            return -1;
+            answer = -1;
         }
         // 목적지에 도착헀다면 stop
-        if (maps[maps.length - 1][maps.length - 1] == 0) {
-            return count;
-        } else {
-            // 사방 탐색
-            findOpenedPlace(maps, x, y, isOpened,count);
+        if (dst > 1) {
+            answer = count;
+        }
+        {
             // 상
-            if (isOpened[0] == true) {
-                x -= 1;
-                maps[x][y] = ++count;
-                changeCoordinate(maps, x, y, new boolean[4], count);
-                --count;
-                x += 1;
+            if (x - 1 >= 0) {
+                if (maps[x - 1][y] == 1 || maps[x - 1][y] > count) {
+                    maps[--x][y] = ++count;
+                    changeCoordinate(maps, x++, y, count--);
+                }
             }
             // 하
-            if (isOpened[1] == true) {
-                x += 1;
-                maps[x][y] = ++count;
-                changeCoordinate(maps, x, y, new boolean[4], count);
-                --count;
-                x -= 1;
+            if (x + 1 <= maps.length - 1) {
+                if (maps[x + 1][y] == 1 || maps[x + 1][y] > count) {
+                    maps[++x][y] = ++count;
+                    changeCoordinate(maps, x--, y, count--);
+                }
             }
             // 좌
-            if (isOpened[2] == true) {
-                y -= 1;
-                maps[x][y] = ++count;
-                changeCoordinate(maps, x, y, new boolean[4], count);
-                --count;
-                y += 1;
+            if (y - 1 >= 0) {
+                if (maps[x][y - 1] == 1 || maps[x][y - 1] > count) {
+                    maps[x][--y] = ++count;
+                    changeCoordinate(maps, x, y++, count--);
+                }
             }
             // 우
-            if (isOpened[3] == true) {
-                y += 1;
-                maps[x][y] = ++count;
-                changeCoordinate(maps, x, y, new boolean[4], count);
-                --count;
-                y -= 1;
+            if (y + 1 <= maps.length - 1) {
+                if (maps[x][y + 1] == 1 || maps[x][y + 1] > count) {
+                    maps[x][++y] = ++count;
+                    changeCoordinate(maps, x, y--, count--);
+                }
             }
         }
-        return 0;
     }
 
 
     public int solution(int[][] maps) {
-        int answer = 0;
-        // 상,하,좌,우
-        boolean[] isOpened = new boolean[4];
 
         // 길의 개수 찾기
         for (int i = 0; i < maps.length; i++) {
@@ -92,11 +60,11 @@ public class Solution {
             }
         }
 
-        maps[0][0] = 0;
-        changeCoordinate(maps, 0, 0, isOpened, 1);
+        maps[0][0] = 1;
+        changeCoordinate(maps, 0, 0, 1);
 
-        System.out.println(Arrays.deepToString(maps));
+        System.out.println(answer);
 
-        return 1;
+        return answer;
     }
 }
