e3268d5ac8a7df6d0cd57d0fd01d389c3ebc1194
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-02-27 11:56:02 +0900

    병렬 처리 실패!

--- java_study/programmers_java/Level2/게임 맵 최단거리/src/com/company/Solution.java
+++ java_study/programmers_java/Level2/게임 맵 최단거리/src/com/company/Solution.java
@@ -1,55 +1,116 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Solution {
-    public int answer = 0;
 
-    public void changeCoordinate(int[][] maps, int x, int y, int count) {
+    public ArrayList<int[]> coordinates = new ArrayList<>();
 
-        if (x == maps.length - 1 && y == maps[0].length - 1) {
-            // 목적지에 도착헀다면 stop
-            int dst = maps[maps.length - 1][maps[0].length - 1];
-            if (dst < answer || answer == 0) {
-                answer = dst;
+    public void checkOpenedPlace(int x, int y, int count, int[][] maps, HashMap<String, Integer> move) {
+        int check = 0;
+        // 상
+        if (x - 1 >= 0) {
+            if (maps[x - 1][y] == 1 || maps[x - 1][y] > count) {
+                move.put("up", 1);
+                check++;
             }
-        } else {
-            // 상
-            if (x - 1 >= 0) {
-                if (maps[x - 1][y] == 1 || maps[x - 1][y] > count) {
-                    maps[--x][y] = ++count;
-                    changeCoordinate(maps, x++, y, count--);
-                }
+        }
+        // 하
+        if (x + 1 <= maps.length - 1) {
+            if (maps[x + 1][y] == 1 || maps[x + 1][y] > count) {
+                move.put("down", 1);
+                check++;
             }
-            // 하
-            if (x + 1 <= maps.length - 1) {
-                if (maps[x + 1][y] == 1 || maps[x + 1][y] > count) {
-                    maps[++x][y] = ++count;
-                    changeCoordinate(maps, x--, y, count--);
-                }
+        }
+        //좌
+        if (y - 1 >= 0) {
+            if (maps[x][y - 1] == 1 || maps[x][y - 1] > count) {
+                move.put("left", 1);
+                check++;
             }
-            // 좌
-            if (y - 1 >= 0) {
-                if (maps[x][y - 1] == 1 || maps[x][y - 1] > count) {
-                    maps[x][--y] = ++count;
-                    changeCoordinate(maps, x, y++, count--);
-                }
+        }
+        if (y + 1 <= maps[0].length - 1) {
+            if (maps[x][y + 1] == 1 || maps[x][y + 1] > count) {
+                move.put("right", 1);
+                check++;
             }
-            // 우
-            if (y + 1 <= maps[0].length - 1) {
-                if (maps[x][y + 1] == 1 || maps[x][y + 1] > count) {
-                    maps[x][++y] = ++count;
-                    changeCoordinate(maps, x, y--, count--);
-                }
+        }
+        if (--check >= 1) {
+            for (int i = 0; i < check; i++) {
+                coordinates.add(new int[]{x, y, count});
             }
         }
     }
 
-
     public int solution(int[][] maps) {
-        changeCoordinate(maps, 0, 0, 1);
-        if (maps[maps.length - 1][maps[0].length - 1] == 1) {
+        int x_max = maps.length - 1;
+        int y_max = maps[0].length - 1;
+        int count = 1;
+        int answer = 0;
+        HashMap<String, Integer> move = new HashMap<>();
+
+        move.put("up", 0);
+        move.put("down", 0);
+        move.put("left", 0);
+        move.put("right", 0);
+
+        maps[0][0] = 0;
+        coordinates.add(new int[]{0, 0, count});
+
+        if(maps[x_max-1][y_max]==0&&maps[x_max][y_max-1]==0)
+        {
             return -1;
-        } else
-            System.out.println(answer);
+        }
+        else{
+            while (maps[x_max][y_max] == 1) {
+                for (int i = 0; i < coordinates.size(); i++) {
+                    int[] coordinate = coordinates.get(i);
+                    int x = coordinate[0];
+                    int y = coordinate[1];
+                    int tmpCount = coordinate[2];
+
+                    if (!move.containsValue(1)) {
+                        // 사방면 체크
+                        checkOpenedPlace(x, y, tmpCount, maps, move);
+                    }
+
+                    // 상
+
+                    if (move.get("up") == 1) {
+                        maps[--x][y] = ++tmpCount;
+                        coordinates.set(i, new int[]{x, y, tmpCount});
+                        move.put("up", 0);
+                        continue;
+                    }
+                    // 하
+                    if (move.get("down") == 1) {
+                        maps[++x][y] = ++tmpCount;
+                        coordinates.set(i, new int[]{x, y, tmpCount});
+                        move.put("down", 0);
+                        continue;
+                    }
+                    // 좌
+                    if (move.get("left") == 1) {
+                        maps[x][--y] = ++tmpCount;
+                        coordinates.set(i, new int[]{x, y, tmpCount});
+                        move.put("left", 0);
+                        continue;
+                    }
+                    // 우
+                    if (move.get("right") == 1) {
+                        maps[x][++y] = ++tmpCount;
+                        coordinates.set(i, new int[]{x, y, tmpCount});
+                        move.put("right", 0);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        answer = maps[x_max][y_max];
+        System.out.println(answer);
+
         return answer;
     }
 }
