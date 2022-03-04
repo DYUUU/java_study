8afb80054e4e95f7b6cac4eff05ab10d75f60b65
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-03-04 15:21:49 +0900

    10점..?!

--- java_study/programmers_java/Level2/순위/src/com/company/Solution.java
+++ java_study/programmers_java/Level2/순위/src/com/company/Solution.java
@@ -1,65 +1,54 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 
 public class Solution {
-    public int solution(int n, int[][] results) {
-        HashMap<Integer, ArrayList<Integer>> rank = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        int frontIdx = 0;
-        int lastIdx = 0;
+    public boolean flag = false;
 
-        for (int i = 0; i < n; i++)
-            rank.put(i+1, new ArrayList<>());
+    public void DFS(boolean[][] table, int x, int y, int n, HashSet<Integer> result) {
+        for (int i = 0; i < n; i++) {
+            // 이기는 경우
+            if (flag) {
+                if (table[x][i]) {
+                    result.add(i);
+                    DFS(table, i, 0, n, result);
+                }
+            }
+            // 지는 경우
+            if (!flag) {
+                if (table[i][y]) {
+                    result.add(i);
+                    DFS(table, 0, i, n, result);
+                }
+            }
+        }
+    }
+
+    public int solution(int n, int[][] results) {
+        boolean[][] table = new boolean[n][n];
+        HashSet<Integer> result = new HashSet<>();
+        int answer = 0;
 
         for (int i = 0; i < n; i++) {
             int winner = results[i][0];
             int loser = results[i][1];
-
-            for (int j = 0; j < n; j++) {
-                // winner
-                // winner가 기존에 있을 때
-                if (rank.get(j + 1).contains(winner)) {
-                    if (frontIdx == 0) {
-                        frontIdx = j + 1;
-                    }
-                }
-                // winner가 기존에 없을 때
-                else {
-                    arr = rank.get(j + 1);
-                    arr.add(winner);
-                    rank.put(j + 1, arr);
-                    if (frontIdx == 0)
-                        frontIdx = j + 1;
-                }
-                // loser
-                // loser가 기존에 없을 때
-                if (!rank.get(j+1).contains(loser)) {
-                    if (frontIdx + j + 1 <= n) {
-                        arr = rank.get(frontIdx + j + 1);
-                        arr.add(loser);
-                        rank.put(frontIdx + j + 1, arr);
-                        lastIdx = frontIdx + j + 1;
-                    }
-                }
-                // loser가 기존에 있을 때
-                else {
-
-                }
-            }
-            if (lastIdx != 0) {
-                if (rank.get(lastIdx).contains(winner)) {
-                    arr = rank.get(lastIdx);
-                    arr.remove(Integer.valueOf(winner));
-                    rank.put(lastIdx, arr);
-                }
-            }
-
-            frontIdx = 0;
-            lastIdx = 0;
+            table[winner - 1][loser - 1] = true;
         }
 
-        return 1;
+        // 가로->세로 DFS
+
+        for (int i = 0; i < n; i++) {
+            flag = true;
+            DFS(table, i, 0, n, result);
+            flag = false;
+            DFS(table, 0, i, n, result);
+            if (result.size() == n - 1) {
+                answer++;
+            }
+            result.clear();
+        }
+
+
+        return answer;
     }
 }
