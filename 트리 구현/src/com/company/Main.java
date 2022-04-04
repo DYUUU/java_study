aee3045709fbda7ae17af4d921b4a32567e2826b
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-04-04 17:10:56 +0900

    first spring test

---
+++ 트리 구현/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+    public class Node{
+        int value;
+        Node left;
+        Node right;
+    }
+
+    public class Tree{
+        public Node root;
+        int size = 0;
+
+        public void setRoot(Node node)
+        {
+            this.root=node;
+        }
+        public Node getRoot(){
+            return root;
+        }
+
+        public Node createNode(Node left)
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
